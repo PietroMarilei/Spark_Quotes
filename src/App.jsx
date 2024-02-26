467c04af8c6fd00c933cb46d858db9e886dbdd22
PietroMarilei <pietromrl0@gmail.com>
2024-02-26 12:02:21 +0100

    main | add write operation

--- src/App.jsx
+++ src/App.jsx
@@ -4,56 +4,92 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Auth } from "./components/auth";
 import { db } from "./config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, addDoc } from "firebase/firestore";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [quotesList, setQuotesList] = useState([]);
 
+  //new quote state
+  const [newQuoteAuthor, setNewQuoteAuthor] = useState("");
+  const [newQuoteText, setNewQuoteText] = useState("");
+  const [newQuoteUrl, setNewQuoteUrl] = useState("");
+
   const quotesCollectionRef = collection(db, "quotes");
+  //read the data
+  const getQuotesList = async () => {
+    try {
+      const data = await getDocs(quotesCollectionRef);
+      //extrac data from data obj
+      const quotes = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      //set  the movie list = data
+      //lancia allo usetState
+      setQuotesList(quotes);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   //read the db
   useEffect(() => {
-    const getQuotesList = async () => {
-      //read the data
-      try {
-        const data = await getDocs(quotesCollectionRef);
-        //extrac data from data obj
-        const quotes = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        //lancia allo usetState, ma react fa schifo e devo mappare
-        setQuotesList(quotes);
-      } catch (error) {
-        console.error(error);
-      }
-      //set  the movie list = data
-    };
-
     getQuotesList();
   }, []);
 
+  const onSubmitQuote = async () => {
+    try {
+      await addDoc(quotesCollectionRef, {
+        author: newQuoteAuthor,
+        text: newQuoteText,
+        url: newQuoteUrl,
+      });
+      //how to empty the values ? 
+      getQuotesList()
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       <Auth />
-
       <h1>SparkQuotes</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+      <h3>Add a Quote</h3>
+
+      <div className="py-3">
+        <input
+          type="text"
+          placeholder="quote author"
+          onChange={(e) => setNewQuoteAuthor(e.target.value)}
+        />
+        {/* meglio textarea in teoria */}
+        <input
+          type="text"
+          placeholder="quote text"
+          onChange={(e) => setNewQuoteText(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="quote url"
+          onChange={(e) => setNewQuoteUrl(e.target.value)}
+        />
+        <button onClick={onSubmitQuote} className="my-3">
+          Add Quote
         </button>
-        <br />
+      </div>
+
+      <div className="card">
         <h2 className="text-lg font-mono  py-2">quotes list :</h2>
-      {/* maps trought quotesList all'arrivo di use effect */}
-        {quotesList.map((quote) => (
-          <>
+        {/* maps trought quotesList all'arrivo di use effect */}
+        {quotesList.map((quote, i) => (
+          <div key={i}>
             <hr />
             <div>id:{quote.id}</div>
             <div>Author:{quote.author}</div>
             <div>Text:{quote.text}</div>
             <div>Url:{quote.url}</div>
             <hr />
-          </>
+          </div>
         ))}
         <p className=" text-red-500 text-lg font-bold py-3">
           Works with Tailwind
